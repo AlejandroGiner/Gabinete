052cf4ba8055822f41e09add2738a08974d93a77
Alejandro Giner <alejandroginerpaez@gmail.com>
2024-01-29 09:36:51 +0100

    start

---
+++ htdocs/api/agregarUsuario.php
@@ -0,0 +1,23 @@
+<?php
+
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+    header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+
+    //echo "estamos en agregarUsuario...";
+    require "./conexion.php";
+
+    $json = file_get_contents("php://input");
+    
+    $objEmpleado = json_decode($json);
+    //echo $objEmpleado;
+
+    $sql = "INSERT INTO usuarios(DNI, Nombre, Apellidos, Telefono) VALUES('$objEmpleado->dni','$objEmpleado->nombre','$objEmpleado->apellidos', '$objEmpleado->telefono')";
+    
+    $query = $mysqli->query($sql);
+
+    //$jsonRespuesta = array('msg2' => 'OK');
+    $jsonRespuesta = array('msg1' => 'OK', 'msg2' => 'Chachi', 'msg3' => 'Guay');
+    echo json_encode($jsonRespuesta);
+    //return $jsonRespuesta;
---
+++ htdocs/api/editarUsuario.php
@@ -0,0 +1,21 @@
+<?php
+
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+    header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+
+    require "conexion.php";
+
+    // recibimos el JSON desde app.js 
+    $json = file_get_contents("php://input");
+    // convertimos el json en una variable php para poderla utilizar en el pgm
+    $objEmpleado = json_decode($json);
+
+    $sql = "UPDATE usuarios SET Nombre='$objEmpleado->nombre', Apellidos='$objEmpleado->apellidos', Telefono='$objEmpleado->telefono' WHERE DNI='$objEmpleado->dni'";
+    
+    $query = $mysqli->query($sql);
+
+    $jsonRespuesta = array('msg' => 'OK');
+    // convertimos la variable php en un json y es lo que enviamos a app.js
+    echo json_encode($jsonRespuesta);
