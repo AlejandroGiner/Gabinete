7f17434f656f4e9cbb3e507ee621a69bdd53d9eb
Alejandro Giner <alejandroginerpaez@gmail.com>
2024-01-29 09:44:28 +0100

    formatting

--- htdocs/api/agregarUsuario.php
+++ htdocs/api/agregarUsuario.php
@@ -1,23 +1,23 @@
 <?php
 
-    header('Access-Control-Allow-Origin: *');
-    header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-    header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
 
-    //echo "estamos en agregarUsuario...";
-    require "./conexion.php";
+//echo "estamos en agregarUsuario...";
+require "./conexion.php";
 
-    $json = file_get_contents("php://input");
-    
-    $objEmpleado = json_decode($json);
-    //echo $objEmpleado;
+$json = file_get_contents("php://input");
 
-    $sql = "INSERT INTO usuarios(DNI, Nombre, Apellidos, Telefono) VALUES('$objEmpleado->dni','$objEmpleado->nombre','$objEmpleado->apellidos', '$objEmpleado->telefono')";
-    
-    $query = $mysqli->query($sql);
+$objEmpleado = json_decode($json);
+//echo $objEmpleado;
 
-    //$jsonRespuesta = array('msg2' => 'OK');
-    $jsonRespuesta = array('msg1' => 'OK', 'msg2' => 'Chachi', 'msg3' => 'Guay');
-    echo json_encode($jsonRespuesta);
+$sql = "INSERT INTO usuarios(DNI, Nombre, Apellidos, Telefono) VALUES('$objEmpleado->dni','$objEmpleado->nombre','$objEmpleado->apellidos', '$objEmpleado->telefono')";
+
+$query = $mysqli->query($sql);
+
+//$jsonRespuesta = array('msg2' => 'OK');
+$jsonRespuesta = array('msg1' => 'OK', 'msg2' => 'Chachi', 'msg3' => 'Guay');
+echo json_encode($jsonRespuesta);
     //return $jsonRespuesta;
--- htdocs/api/conexion.php
+++ htdocs/api/conexion.php
@@ -1,11 +1,11 @@
 <?php
-    
-    // Variables de la conexion a la DB
-    $mysqli = new mysqli("mysql_iaw","root","root","gabinetedb");
-    // echo "intentando conectar....";
-    // Comprobamos la conexion
-    if($mysqli->connect_errno) {
-        die("Fallo la conexion");
-    } else {
-        //echo "Conexion exitosa";
-    }
+
+// Variables de la conexion a la DB
+$mysqli = new mysqli("mysql_iaw", "root", "root", "gabinetedb");
+// echo "intentando conectar....";
+// Comprobamos la conexion
+if ($mysqli->connect_errno) {
+    die("Fallo la conexion");
+} else {
+    //echo "Conexion exitosa";
+}
