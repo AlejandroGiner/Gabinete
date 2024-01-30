c739d4d61025012a2724bdb1c44adc224cf2a51f
Alejandro Giner <alejandroginerpaez@gmail.com>
2024-01-30 14:04:46 +0100

    modals etc

--- htdocs/api/obtenerCita.php
+++ htdocs/api/obtenerCita.php
@@ -1,22 +1,30 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+    header("Allow: GET, POST, OPTIONS, PUT, DELETE");
 
-require "conexion.php";
+    require "conexion.php";
 
-// PRUEBAS
-//$usuario = "cheko";
+    $json = file_get_contents("php://input");
+    
+    $objId = json_decode($json);
 
-$sql = "SELECT * FROM citas";
-$query = $mysqli->query($sql);
+    $sql = "SELECT * FROM citas WHERE DNI='$objId->idUsuario'";
+    
+    // PRUEBAS
+    //$usuario = "cheko";
+    
+    //$sql = "SELECT * FROM citas ";
 
-$datos = array();
 
-while ($resultado = $query->fetch_assoc()) {
-    $datos[] = $resultado;
-}
-
-echo json_encode($datos);
+    $query = $mysqli->query($sql);
+    
+    $datos = array();
+    
+    while($resultado = $query->fetch_assoc()) {
+        $datos[] = $resultado;
+    }
+        
+    echo json_encode($datos);
--- htdocs/app2.js
+++ htdocs/app2.js
@@ -1,56 +1,90 @@
-const urlObtenerUsuario = '/api/obtenerUsuario.php';
-const urlAgregarUsuario = '/api/agregarUsuario.php';
-const urlEditarUsuario = '/api/editarUsuario.php';
-const urlBorrarUsuario = '/api/borrarUsuario.php';
+const URL_GET_USER = '/api/obtenerUsuario.php';
+const URL_ADD_USER = '/api/agregarUsuario.php';
+const URL_EDIT_USER = '/api/editarUsuario.php';
+const URL_DELETE_USER = '/api/borrarUsuario.php';
 
-async function obtenerEmpleados() {
+/**
+ * Generates a td element with a value inside.
+ * @param {string} value - The text contained by the cell.
+ * @returns 
+ */
+function generateTextCell(value) {
+    const cell = document.createElement('td');
+    cell.innerText = value;
+    return cell;
+}
+
+/**
+ * Generates a button element with specified classes, Bootstrap toggle, Bootstrap target and text.
+ * @param {Array<string>} classes 
+ * @param {string} target 
+ * @param {string} text 
+ * @param {string} toggle 
+ * @returns 
+ */
+function generateButton(classes, target, text, toggle = 'modal') {
+    const button = document.createElement('button')
+    button.classList.add(...classes)
+    button.setAttribute('data-bs-toggle', toggle)
+    button.setAttribute('data-bs-target', target)
+    button.innerText = text
+    return button
+}
+
+/**
+ * Generates a cell containing a specified button.
+ * @param {*} button 
+ * @returns 
+ */
+function generateButtonCell(button) {
+    const cell = document.createElement('td');
+    cell.appendChild(button);
+    return cell;
+}
+
+/**
+ * Generates a row for a table based on a specific user.
+ * @param {*} user 
+ * @returns 
+ */
+function generateUserRow(user) {
+    const userRow = document.createElement('tr')
+    userRow.appendChild(generateTextCell(user.Nombre))
+    userRow.appendChild(generateTextCell(user.Apellidos))
+    userRow.appendChild(generateTextCell(user.Telefono))
+
+    modifyButton = generateButton(['btn', 'btn-secondary'], '#modificarUsuarioModal', 'Modificar')
+    deleteButton = generateButton(['btn', 'btn-danger'], '#eliminarUsuarioModal', 'Eliminar')
+    showAppointmentsButton = generateButton(['btn', 'btn-primary'], '#verCitasModal', 'Ver Citas')
+
+    userRow.appendChild(generateButtonCell(modifyButton))
+    userRow.appendChild(generateButtonCell(deleteButton))
+    userRow.appendChild(generateButtonCell(showAppointmentsButton))
+
+    return userRow
+}
+
+/**
+ * Populates the table body with user rows.
+ */
+async function getUsers() {
     try {
-        const res = await fetch(urlObtenerUsuario,
-            {
-                method: 'GET',
+        const response = await fetch(URL_GET_USER);
+        users = await response.json()
 
-            });
+        const tbody = document.querySelector('tbody')
 
-        //console.log(`datos devueltos primer fetch${res}`)
-        listaEmpleados = await res.json()
-        console.log(listaEmpleados)
+        for (user of users) {
 
-        let keysinobj = Object.keys(listaEmpleados)
-        parrafo = document.getElementById("demo")
-        cadena = ""
-        if (keysinobj.length == 0) {
-
-            cadena = "<h3>No Hay Pacientes en la BBDD</h3>"
-            parrafo.innerHTML = cadena
-
-        } else {
-            //mostrarEmpleados()
-
-            listaEmpleados.forEach(empleado => {
-                //const {dniUsuario, nombre, apellidos, telefono} = empleado
-
-                console.log(empleado)
-
-                cadena += "<tr><td> " + empleado.DNI + "</td><td> " + empleado.Nombre + "</td><td> " + empleado.Apellidos + "</td><td> " + empleado.Telefono + "</td>"
-
-                cadena += "<td><button onclick=\"eliminarEmpleado('" + empleado.DNI + "')\" class=\"btn btn-eliminar\">Eliminar</button></td>"
-                cadena += "<td><button onclick=\"cargarEmpleado('" + empleado.DNI + "','" + empleado.Nombre + "','" + empleado.Apellidos + "','" + empleado.Telefono + "')\" class=\"btn btn-editar\">Modificar</button></tr></td>"
-                //<button onclick="eliminarEmpleado(${idUsuario})" class="btn btn-eliminar">Eliminar</button>
-                //<button onclick="cargarEmpleado(${idUsuario}, '${nombre}', '${contrasena}', '${email}')" class="btn btn-editar">Modificar</button>
-
-
-            })
-            cadena2 = "<table class= \"table table-striped\">" + cadena + "</table>"
-            console.log(cadena2)
-            parrafo.innerHTML = cadena2
+            const userRow = generateUserRow(user)
+            tbody.appendChild(userRow)
 
         }
-
     }
+
     catch (error) {
         console.log(error)
     }
-
 }
 
-obtenerEmpleados()
+getUsers()
